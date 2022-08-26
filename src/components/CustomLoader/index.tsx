d36be65e5dfd762555a2b7243bc867e76cf7e288
sapar6ek0v <razetka0309@gmail.com>
2022-08-26 08:59:38 +0300

    added Custom Loader

---
+++ src/components/CustomLoader/index.tsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import { Loader } from '@mantine/core';
+
+const CustomLoader = () => {
+  return (
+    <div className="fixed top-1/2	left-1/2 translate-x-1/2 translate-y-1/2">
+      <Loader />
+    </div>
+  );
+};
+
+export default CustomLoader;
